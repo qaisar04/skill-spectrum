ec558b02d1d2e6f555920f7b01569dc182e77d02
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-22 22:13:05 +0600

    init grading-service

---
+++ backend/grading-service/src/main/java/kz/balatabayev/gradingservice/GradingServiceApplication.java
@@ -0,0 +1,11 @@
+package kz.balatabayev.gradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GradingServiceApplication.class, args);
+    }
+}
