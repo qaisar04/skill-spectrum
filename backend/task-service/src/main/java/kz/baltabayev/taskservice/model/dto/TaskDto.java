79231f516a8b4610af7f5123477cfe9aadc17ce7
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-22 10:12:53 +0600

    bugfix

--- backend/task-service/src/main/java/kz/baltabayev/taskservice/model/dto/TaskDto.java
+++ backend/task-service/src/main/java/kz/baltabayev/taskservice/model/dto/TaskDto.java
@@ -1,9 +1,13 @@
 package kz.baltabayev.taskservice.model.dto;
 
 import kz.baltabayev.taskservice.model.types.Status;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class TaskDto {
     private String title;
     private String description;
